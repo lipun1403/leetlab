ea99c25d5331a012753a77a24cce966b345d6e74
lipun <lenkapradyumna1404@gmail.com>
2026-04-02 07:39:16 +0530

    feat: initialize frontend with Vite, React, and Tailwind CSS setup
    - Add Vite configuration for React and Tailwind CSS
    - Create main entry point for React application
    - Implement global styles with Tailwind CSS
    - Add pages for Home, Login, Sign Up, Add Problem, and Problem details
    - Create components for problem management and submissions
    - Integrate form handling with React Hook Form and Zod for validation
    - Add SVG logo for Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import SignUpPage from "./page/SignUpPage.jsx";
+
+const App = () => {
+  let authUser = null
+
+  return (
+    <div className='flex flex-col items-center justify-start'>
+      <Routes>
+        <Route path='/' element={ authUser?<HomePage/>:<Navigate to={"/login"}/>}/>
+        <Route path='/login' element={ !authUser?<LoginPage/>:<Navigate to={"/"}/>}/>
+        <Route path='/signup' element={ !authUser?<SignUpPage/>:<Navigate to={"/"}/>}/>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
